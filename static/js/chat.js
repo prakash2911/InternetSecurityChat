1aaf6b8d9f23b5a0588f97c2d1c0ffafc3bf785b
prakash2911 <prakash29112002@gmail.com>
2022-09-24 23:51:59 +0530

    update date

--- static/js/chat.js
+++ static/js/chat.js
@@ -79,3 +79,13 @@ const sendMessage = (msg) => {
 	input.value = '';
 	input.focus();
 };
+
+// Getting current date
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+var yyyy = today.getFullYear();
+today = dd + "/" +mm + "/" + yyyy;
+document.getElementById("date").innerText=today
+
